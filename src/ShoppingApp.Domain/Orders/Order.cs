10bdf73b83a9b05e6101284c3e276874a51337a2
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-06-14 19:09:38 +0200

    Added domain models

---
+++ src/ShoppingApp.Domain/Orders/Order.cs
@@ -0,0 +1,11 @@
+﻿using ShoppingApp.Domain.SeedWork;
+using System;
+
+namespace ShoppingApp.Domain.Orders
+{
+    public class Order : IAggregateRoot
+    {
+        public Guid OrderId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ src/ShoppingApp.Domain/Orders/OrderProduct.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingApp.Domain.Products;
+using System;
+
+namespace ShoppingApp.Domain.Orders
+{
+    public class OrderProduct
+    {
+        public Guid OrderProductId { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
