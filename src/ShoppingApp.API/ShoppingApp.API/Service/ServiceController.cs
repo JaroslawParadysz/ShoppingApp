edb66569812837ad6135f9057503a6244b6ee5b9
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-06-15 19:28:16 +0200

    Added service controller

--- src/ShoppingApp.API/ShoppingApp.API/Service/ServiceController.cs
+++ src/ShoppingApp.API/ShoppingApp.API/Service/ServiceController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ShoppingApp.API.Service
 {
@@ -11,5 +6,11 @@ namespace ShoppingApp.API.Service
     [ApiController]
     public class ServiceController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult PopulateDb()
+        {
+
+            return Created(string.Empty, null);
+        }
     }
 }
