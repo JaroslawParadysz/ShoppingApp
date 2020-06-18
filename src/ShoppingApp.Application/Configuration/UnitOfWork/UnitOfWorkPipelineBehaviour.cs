85009d3cda57f67d8e7554f8d82fc7b5d59bf346
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-06-18 06:00:13 +0200

    Db context inside unit of work

--- src/ShoppingApp.Application/Configuration/UnitOfWork/UnitOfWorkPipelineBehaviour.cs
+++ src/ShoppingApp.Application/Configuration/UnitOfWork/UnitOfWorkPipelineBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using ShoppingApp.Infrastructure.SqlServer.Database;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ namespace ShoppingApp.Application.Configuration.UnitOfWork
 {
     public class UnitOfWorkPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
+        private readonly ShoppingAppContext _shoppingAppContext;
+        public UnitOfWorkPipelineBehaviour(ShoppingAppContext shoppingAppContext)
+        {
+            _shoppingAppContext = shoppingAppContext;
+        }
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             Trace.WriteLine("before");
