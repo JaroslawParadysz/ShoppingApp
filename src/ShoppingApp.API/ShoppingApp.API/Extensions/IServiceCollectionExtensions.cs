f2b50ed6c1f35c99a1507bf502e16681fc22231a
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-06-18 05:54:19 +0200

    Added extension for CQRS based on MediatR

---
+++ src/ShoppingApp.API/ShoppingApp.API/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingApp.Application.Configuration.Commands;
+using ShoppingApp.Application.Configuration.UnitOfWork;
+
+namespace ShoppingApp.API.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCQRS(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(ICommand));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnitOfWorkPipelineBehaviour<,>));
+            return services;
+        }
+    }
+}
--- src/ShoppingApp.API/ShoppingApp.API/Startup.cs
+++ src/ShoppingApp.API/ShoppingApp.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingApp.API.Extensions;
 using ShoppingApp.Application.Configuration.Commands;
 using ShoppingApp.Application.Configuration.UnitOfWork;
 using ShoppingApp.Infrastructure.SqlServer.Database;
@@ -27,8 +28,7 @@ namespace ShoppingApp.API
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"));
             });
             services.AddControllers();
-            services.AddMediatR(typeof(ICommand));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnitOfWorkPipelineBehaviour<,>));
+            services.AddCQRS();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
