6991ed90f7df996046803e8ba1c9de3356b76d26
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-06-30 22:10:52 +0200

    Order details component

--- src/UI/ShoppingApp/src/app/app-routing.module.ts
+++ src/UI/ShoppingApp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { OrderDetailsComponent } from './order-details/order-detials.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { OrderListComponent } from './order-list/order-list.component';
 
 const routes: Routes = [
-  {path: '', component: OrderListComponent }
+  { path: '', component: OrderListComponent },
+  { path: 'order/:orderId', component: OrderDetailsComponent }
 ];
 
 @NgModule({
