b7096e671d3a7a661175a8b38c8a9bc0f3baafa1
Jaroslaw Paradysz <jaroslaw.paradysz@gmail.com>
2020-07-06 20:19:16 +0200

    basic authetication

---
+++ src/API/ShoppingApp.Authentication/Models/Identity/RegisterUserRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingApp.Authentication.Models.Identity
+{
+    public class RegisterUserRequestModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
